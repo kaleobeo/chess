a991994e7ca71cc205ebbae10603384d70f913e6
Kaleo K <kaleo.kronberg@gmail.com>
2022-07-14 16:12:13 -0700

    Rewrite tests to use with FEN and Rook objects

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -16,8 +16,8 @@ describe Board do
     context 'when the cell is empty' do
       subject(:empty_piece_at_board) { described_class.new }
 
-      it 'returns nil' do
-        expect(empty_piece_at_board.piece_at(Position.parse('d4'))).to be_nil
+      it 'returns a null piece' do
+        expect(empty_piece_at_board.piece_at(Position.parse('d4'))).to be_a(NullPiece)
       end
     end
 
@@ -61,24 +61,24 @@ describe Board do
   end
 
   describe '#move' do
-    subject(:move_board) { described_class.new }
+    subject(:move_board) { described_class.parse_fen('8/8/8/1r1R4/1r6/8/8/8 w - - 0 1') }
 
     context 'when destination and target are the same' do
       let(:move) { double('move') }
 
+
       before do
-        move_board.place_piece(Position.parse('d4'), 'n')
-        allow(move).to receive_messages(from: Position.parse('d4'), to: Position.parse('d5'), target: Position.parse('d5'))
+        allow(move).to receive_messages(from: Position.parse('d5'), to: Position.parse('b5'), target: Position.parse('b5'))
       end
 
       it 'changes the destination cell\'s piece' do
         move_board.move(move)
-        expect(move_board.piece_at(Position.parse('d5'))).to eq 'n'
+        expect(move_board.piece_at(Position.parse('b5'))).to eq Piece.parse('R', Position.parse('b5'))
       end
 
       it 'clears the cell that was moved from' do
         move_board.move(move)
-        expect(move_board.piece_at(Position.parse('d4'))).to be_nil
+        expect(move_board.piece_at(Position.parse('d4'))).to be_a(NullPiece)
       end
     end
 
@@ -86,24 +86,22 @@ describe Board do
       let(:move) { double('move') }
 
       before do
-        move_board.place_piece(Position.parse('d4'), 'n')
-        move_board.place_piece(Position.parse('e4'), 'p')
-        allow(move).to receive_messages(from: Position.parse('d4'), to: Position.parse('e5'), target: Position.parse('e4'))
+        allow(move).to receive_messages(from: Position.parse('d5'), to: Position.parse('b5'), target: Position.parse('b4'))
       end
 
       it 'changes the destination cell\'s piece' do
         move_board.move(move)
-        expect(move_board.piece_at(Position.parse('e5'))).to eq 'n'
+        expect(move_board.piece_at(Position.parse('b5'))).to eq Piece.parse('R', Position.parse('b5'))
       end
 
       it 'clears the cell that was moved from' do
         move_board.move(move)
-        expect(move_board.piece_at(Position.parse('d4'))).to be_nil
+        expect(move_board.piece_at(Position.parse('d4'))).to be_a(NullPiece)
       end
 
       it 'clears the target cell' do
         move_board.move(move)
-        expect(move_board.piece_at(Position.parse('e4'))).to be_nil
+        expect(move_board.piece_at(Position.parse('e4'))).to be_a(NullPiece)
       end
     end
   end
@@ -112,11 +110,39 @@ describe Board do
     context 'when given the fen \'8/4r3/8/8/8/8/8/8\'' do
       subject(:board) { described_class.parse_fen('8/4r3/8/8/8/8/8/8') }
 
-      it 'has a rook at e7' do
-        expect(board.piece_at(Position.parse('e7'))).to eq 'r'
+      it 'has a black rook at e7' do
+        expect(board.piece_at(Position.parse('e7'))).to eq Piece.parse('r', Position.parse('e7'))
+      end
+
+      it 'all other squares are empty' do
+        empty_squares = board.instance_variable_get(:@board).values.count(&:empty?)
+        expect(empty_squares).to eq 63
       end
     end
 
-    context 'when given '
+    context 'when given the fen \'8/4r2R/3r4/8/8/8/r7/8\'' do
+      subject(:board) { described_class.parse_fen('8/4r2R/3r4/8/8/8/R7/8') }
+
+      it 'has a black rook at e7' do
+        expect(board.piece_at(Position.parse('e7'))).to eq Piece.parse('r', Position.parse('e7'))
+      end
+
+      it 'has a black rook at d6' do
+        expect(board.piece_at(Position.parse('d6'))).to eq Piece.parse('r', Position.parse('d6'))
+      end
+
+      it 'has a white rook at h7' do
+        expect(board.piece_at(Position.parse('h7'))).to eq Piece.parse('R', Position.parse('h7'))
+      end
+
+      it 'has a white rook at a2' do
+        expect(board.piece_at(Position.parse('a2'))).to eq Piece.parse('R', Position.parse('a2'))
+      end
+
+      it 'all other squares are empty' do
+        empty_squares = board.instance_variable_get(:@board).values.count(&:empty?)
+        expect(empty_squares).to eq 60
+      end
+    end
   end
 end
