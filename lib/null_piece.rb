a9bb1d0183c33fb4f40eb53758ab3a5a5bb63aa3
Kaleo K <kaleo.kronberg@gmail.com>
2022-07-18 11:37:00 -0700

    Add NullPiece and NullPiece tests

---
+++ lib/null_piece.rb
@@ -0,0 +1,18 @@
+class NullPiece < Piece
+  def initialize(color = nil, pos = nil, *args)
+    @color = nil
+    @pos = pos
+  end
+
+  def self.represented_by?(_string)
+    true
+  end
+
+  def moves
+    []
+  end
+
+  def friendly_to?(_attacker)
+    false
+  end
+end
---
+++ spec/null_piece_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require_relative '../lib/library'
+
+describe NullPiece do
+  describe '#self.represented_by?' do
+    it 'returns true to an invalid letter' do
+      outcome = described_class.represented_by?('X')
+      expect(outcome).to eq true
+    end
+
+    it 'returns true to an invalid symbol' do
+      outcome = described_class.represented_by?('-')
+      expect(outcome).to eq true
+    end
+  end
+
+  describe 'moves' do
+    subject(:move_piece) { described_class.new }
+
+    it 'returns an empty array' do
+      expect(move_piece.moves).to eq []
+    end
+  end
+
+  describe '#friendly_to?' do
+    subject(:friendly_null_piece) { described_class.new }
+
+    it 'returns false to a white piece' do
+      attacker = Piece.parse('R', Position.parse('a1'), Board.new)
+      expect(friendly_null_piece.friendly_to?(attacker)).to eq false
+    end
+
+    it 'returns false to a black piece' do
+      attacker = Piece.parse('r', Position.parse('a1'), Board.new)
+      expect(friendly_null_piece.friendly_to?(attacker)).to eq false
+    end
+  end
+end
