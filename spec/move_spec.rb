f84766bdac88aaa18dc641e0fd6768ec02eca133
Kaleo K <kaleo.kronberg@gmail.com>
2022-07-18 12:15:51 -0700

    Add Move tests
    Move generation testing handling will be handled in pieces

---
+++ spec/move_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require_relative '../lib/library'
+
+describe Move do
+  describe '#follows_rules?' do
+    context 'when one of a move\'s rules is broken by the board state' do
+      subject(:illegal_move) { described_class.new(Position.parse('a1'), Position.parse('c1'), [Rules::COLLISION, Rules::FRIENDLY_FIRE] )}
+
+      it 'returns false' do
+        expect(illegal_move.follows_rules?(Board.parse_fen('8/8/8/8/8/8/8/1R6 w - - 0 1'))).to eq false
+      end
+    end
+
+    context 'when none of a move\'s rules are broken by the board state' do
+      subject(:legal_move) { described_class.new(Position.parse('a1'), Position.parse('c1'), [Rules::COLLISION, Rules::FRIENDLY_FIRE] ) }
+
+      it 'returns true' do
+        expect(legal_move.follows_rules?(Board.parse_fen('8/8/8/8/8/8/8/8 w - - 0 1'))).to eq true
+      end
+    end
+  end
+
+  describe '#==' do
+    subject(:move) { described_class.new(Position.parse('a1'), Position.parse('a3'), [Rules::COLLISION]) }
+
+    context 'when objects share the same from, to, target, and rules attributes' do
+      it 'returns true' do
+        other = described_class.new(Position.parse('a1'), Position.parse('a3'), [Rules::COLLISION])
+        expect(move == other).to eq true
+      end
+    end
+
+    context 'when objects have one or more discrepancies between their from, to, target, and rules' do
+      it 'returns false' do
+        other = described_class.new(Position.parse('a1'), Position.parse('a4'), [Rules::COLLISION])
+        expect(move == other).to eq false
+      end
+    end
+  end
+end
