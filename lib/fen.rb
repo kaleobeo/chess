161f78f624da9f853b9431a922f6c2ee74a68239
Kaleo K <kaleo.kronberg@gmail.com>
2022-07-26 13:42:27 -0700

    Refactor Fen, add en passant test in fen_spec

--- lib/fen.rb
+++ lib/fen.rb
@@ -1,21 +1,37 @@
 # frozen_string_literal: true
 
+# tested in board_spec
 class Fen
   attr_reader :board
-  def initialize(fen_string)
+  attr_reader :fen_arr
+
+  def initialize(fen_string = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
     @board = Board.new
     @fen_string = fen_string
+    @fen_arr = fen_string.split
   end
 
   def self.load(fen_string)
     fen = new(fen_string)
-    fen_arr = fen_string.split
+    fen.place_pieces
+    fen.place_en_passant
+    fen.board
+  end
+
+  def place_pieces
     piece_placement_arr = fen_arr[0].split('/').reverse
     (1..8).each do |row_idx|
       row_str = piece_placement_arr[row_idx - 1]
-      fen.place_row_from_fen(row_idx, row_str)
+      place_row_from_fen(row_idx, row_str)
     end
-    fen.board
+    self
+  end
+
+  def place_en_passant
+    en_passant_field = fen_arr[3]
+    dir = en_passant_field[1].to_i > 4 ? -1 : 1
+    pawn_pos = Position.parse(en_passant_field).up(dir)
+    @board.piece_at(pawn_pos).moved(Move.new(pawn_pos, pawn_pos, [Rules::ON_START_POSITION]))
   end
 
   def place_row_from_fen(row_num, row_str)
---
+++ spec/fen_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require_relative '../lib/library'
+
+describe Fen do
+  describe '#place_en_passant' do
+    context 'when given [..., ..., ..., e3 ...]' do
+      subject(:en_passant_fen) { described_class.new('8/8/8/8/4P3/8/8/8 w - e3 0 1').place_pieces }
+
+      it 'changes a pawn on e4 to be targetable by attack en passant' do
+        en_passant_fen.place_en_passant
+        expect(en_passant_fen.board.piece_at(Position.parse('e4')).can_en_passant? ).to be true
+      end
+    end
+  end
+end
